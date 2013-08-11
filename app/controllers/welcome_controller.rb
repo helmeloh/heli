a6bd63878c9baa79189365568e2776cd9d17fd6f
Helmut Hirner <helmut@hirner.at>
2013-08-12 00:52:47 +0200

    Repository initialisieren

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,5 @@
+class WelcomeController < ApplicationController
+  def index
+    @text = "jaja heli"
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Heli::Application.config.secret_key_base = 'f83fa1eba718252bec14da8d02d8c2d9b74a9b415f15802fe8e4949e9cb6c1de8a2d5e696ad5fb2240ac33900c695ce04acb697c4b7dcc3060ecdf7a2ec2e013'
