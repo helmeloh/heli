2fdaec3c6858107e597b32760728451293adc5e3
Helmut Hirner <helmut@hirner.at>
2013-09-02 00:25:33 +0200

    singn in and out with tests

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -25,17 +25,18 @@ class UsersController < ApplicationController
   # POST /users.json
   def create
     @user = User.new(user_params)
-
-    respond_to do |format|
-      if @user.save
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
+ # respond_to do |format|
+    if @user.save
+      sign_in @user
+      flash[:success] = "Welcome to the Heli!"
         format.json { render action: 'show', status: :created, location: @user }
-      else
-        format.html { render action: 'new' }
+      redirect_to @user
+    else
+      render 'new'
+      format.html { render action: 'new' }
         format.json { render json: @user.errors, status: :unprocessable_entity }
       end
-    end
-  end
+   end
 
   # PATCH/PUT /users/1
   # PATCH/PUT /users/1.json
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,17 @@ class User < ActiveRecord::Base
 	has_secure_password
 	validates_presence_of :password, :on => :create
 	:password_digest
+	def User.new_remember_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def User.encrypt(token)
+    Digest::SHA1.hexdigest(token.to_s)
+  end
+
+  private
+
+    def create_remember_token
+      self.remember_token = User.encrypt(User.new_remember_token)
+    end
 end
