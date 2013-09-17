50e83895fed6254fb42da9fc6426c553a641cae1
Helmut Hirner <helmut@hirner.at>
2013-09-17 02:35:22 +0200

    admin und delete user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,8 @@
 class UsersController < ApplicationController
  before_action :set_user, only: [:show ]
-   before_action :signed_in_user, only: [:index, :edit, :update]
+  before_action :signed_in_user, only: [:index, :edit, :update, :destroy]
   before_action :correct_user,   only: [:edit, :update]
+  before_action :admin_user,     only: :destroy
 
   # GET /users
   # GET /users.json
@@ -34,19 +35,6 @@ class UsersController < ApplicationController
     end
   end
 
-  # PATCH/PUT /users/1
-  # PATCH/PUT /users/1.json
-  #def update
-  #  respond_to do |format|
-  #    if @user.update(user_params)
-  #      format.html { redirect_to @user, notice: 'User was successfully updated.' }
-  #      format.json { head :no_content }
-  #    else
-  #      format.html { render action: 'edit' }
-  #      format.json { render json: @user.errors, status: :unprocessable_entity }
-  #    end
-  #  end
-  #end
   def update
     @user = User.find(params[:id])
     if @user.update_attributes(user_params)
@@ -60,14 +48,11 @@ class UsersController < ApplicationController
 def edit
     @user = User.find(params[:id])
 end
-  # DELETE /users/1
-  # DELETE /users/1.json
+
   def destroy
-    @user.destroy
-    respond_to do |format|
-      format.html { redirect_to users_url }
-      format.json { head :no_content }
-    end
+    User.find(params[:id]).destroy
+    flash[:success] = "User destroyed."
+    redirect_to users_url
   end
 
   private
@@ -94,4 +79,8 @@ end
       @user = User.find(params[:id])
       redirect_to(root_path) unless current_user?(@user)
     end
+    
+    def admin_user
+      redirect_to(root_url) unless current_user.admin?
+    end    
 end
